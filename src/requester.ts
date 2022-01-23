ce7f37325ea60d40b5b1a7a2913c47e99cd272ef
cengiz berat dinckan <cedinckan@gmail.com>
2022-01-23 22:37:58 +0300

    parameters changed

--- src/requester.ts
+++ src/requester.ts
@@ -1,6 +1,26 @@
+import { IClientPublishOptions } from "mqtt";
+import { publishWithResponseBasic } from "./helpers/mqtt-async.helper";
 import mqttServerClient from "./utils/connectMqtt";
 
 const deviceName: string = "device_1";
 const relayName: string = "relay_1";
 
-setTimeout(() => {});
+setTimeout(async () => {
+  const responseTopic = `response/${deviceName}/${relayName}`;
+  const requestTopic = `request/${deviceName}/${relayName}`;
+  const publishOptions: IClientPublishOptions = {
+    qos: 1,
+    properties: {
+      responseTopic,
+      correlationData: Buffer.from("test", "utf-8"),
+    },
+  };
+  const responseMessage = await publishWithResponseBasic({
+    client: mqttServerClient,
+    publishOptions,
+    requestTopic,
+    responseTopic,
+    message: 1,
+  });
+  console.log(responseMessage);
+}, 1000);
