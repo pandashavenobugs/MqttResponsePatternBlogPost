b1e27b0bcd8932307dcc0017cbf07b6c3188758f
cengiz berat dinckan <cedinckan@gmail.com>
2022-01-30 20:39:29 +0300

    switch case and start system added

--- src/requester.ts
+++ src/requester.ts
@@ -1,4 +1,7 @@
 import EventEmitter from "events";
+import { IClientPublishOptions, MqttClient } from "mqtt";
+import { publishWithResponse } from "./helpers/mqtt-async.helper";
+import { relayResponseEvent } from "./helpers/relayResponseEvent.helper";
 import mqttServerClient from "./utils/connectMqtt";
 
 const eventEmitter = new EventEmitter();
@@ -7,7 +10,69 @@ const eventEmitter = new EventEmitter();
 mqttServerClient.subscribe("response/+/+");
 mqttServerClient.subscribe("otherTopics/#");
 mqttServerClient.on("message", (topic, payload) => {
-  const topicArr = topic.split("/"); //spliting the topic
-  if (topicArr[0] === "response") {
+  const topicArr = topic.split("/"); //spliting the topic ==> [response,deviceName,relayName]
+  switch (topicArr[0]) {
+    case "response":
+      return relayResponseEvent({
+        eventEmitter,
+        deviceName: topicArr[1],
+        relayName: topicArr[2],
+        payload,
+      });
+    case "otherTopics":
+      console.log("other topics");
+      return;
+    default:
+      return console.log("can not find anything");
   }
 });
+
+const startSystem = () => {
+  startResponsePatternExample({
+    deviceName: "device_1",
+    relayName: "relay_1",
+    message: 1,
+  });
+  startResponsePatternExample({
+    deviceName: "device_2",
+    relayName: "relay_1",
+    message: 1,
+  });
+};
+
+const startResponsePatternExample = async ({
+  deviceName,
+  relayName,
+  message,
+}: {
+  deviceName: string;
+  relayName: string;
+  message: 1 | 0;
+}) => {
+  try {
+    const responseTopic = `response/${deviceName}/${relayName}`;
+    const requestTopic = `request/${deviceName}/${relayName}`;
+    const responseEventName = `responseEvent/${deviceName}/${relayName}`;
+    const publishOptions: IClientPublishOptions = {
+      qos: 1,
+      properties: {
+        responseTopic,
+        correlationData: Buffer.from("secret", "utf-8"),
+      },
+    };
+    const responseMessage = await publishWithResponse({
+      client: mqttServerClient,
+      data: message,
+      publishOptions,
+      requestTopic,
+      responseEventName,
+      eventEmitter,
+    });
+    console.log(`${deviceName}/${relayName} : ${responseMessage.message}`);
+  } catch (error) {
+    console.log(`${deviceName}/${relayName} : ${error}`);
+  }
+};
+setTimeout(() => {
+  startSystem();
+}, 1000);
